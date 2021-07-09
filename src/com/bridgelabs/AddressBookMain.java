8e536ada40bd58b735616b0d4df1ba2732b6ecd5
SamarthBM <samarthbm18@gmail.com>
2021-07-09 23:54:10 +0530

    Initial commit

---
+++ src/com/bridgelabs/AddressBookMain.java
@@ -0,0 +1,16 @@
+/****************************************************************
+ * Purpose : To create a address book of a user.
+ * @author Samarth BM
+
+***************************************************************/
+
+package com.bridgelabs;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book program ");
+
+	}
+
+}
