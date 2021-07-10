811feb438e11b0e923775393e45e4f77dfc351db
SamarthBM <samarthbm18@gmail.com>
2021-07-10 20:42:11 +0530

    Added: Method to edit user details

--- src/com/bridgelabs/AddressBookMain.java
+++ src/com/bridgelabs/AddressBookMain.java
@@ -86,7 +86,7 @@ class contactInfo {
 }
 
 public class AddressBookMain {
-	private static Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 	static ArrayList<contactInfo> arrayPersonDetails = new ArrayList<>();
 
 	/*
@@ -113,6 +113,66 @@ public class AddressBookMain {
 		contact_details.setEmail(sc.next());
 
 		arrayPersonDetails.add(contact_details);
+
+	}
+
+	/*
+	 * Method to Edit contact details by confirming first name of a person and
+	 * storing edited details in an array.
+	 */
+
+	public void editDetails() {
+		System.out.println("Confirm your first name to edit details: ");
+		String confirm_name = sc.next();
+
+		for (int i = 0; i < arrayPersonDetails.size(); i++) {
+			if (arrayPersonDetails.get(i).getFirstName().equals(confirm_name)) {
+				System.out.println("Select form below to change: ");
+				System.out.println(
+						"\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Mobile number\n8.Email");
+				int edit = sc.nextInt();
+
+				switch (edit) {
+				case 1:
+					System.out.println("Enter new first name");
+					arrayPersonDetails.get(i).setFirstName(sc.next());
+					break;
+				case 2:
+					System.out.println("Enter new Last name");
+					arrayPersonDetails.get(i).setLastName(sc.next());
+					break;
+				case 3:
+					System.out.println("Enter new Address");
+					arrayPersonDetails.get(i).setAddress(sc.next());
+					break;
+				case 4:
+					System.out.println("Enter new City");
+					arrayPersonDetails.get(i).setCity(sc.next());
+					break;
+				case 5:
+					System.out.println("Enter new State");
+					arrayPersonDetails.get(i).setState(sc.next());
+					break;
+				case 6:
+					System.out.println("Enter new Zip");
+					arrayPersonDetails.get(i).setZip(sc.nextLong());
+					break;
+				case 7:
+					System.out.println("Enter new Mobile number");
+					arrayPersonDetails.get(i).setPhoneNumber(sc.nextLong());
+					break;
+				case 8:
+					System.out.println("Enter new E-mail");
+					arrayPersonDetails.get(i).setEmail(sc.next());
+					break;
+				}
+				System.out.println("Edited list is: ");
+				System.out.println(arrayPersonDetails);
+
+			} else
+				System.out.println("Enter valid First name");
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -120,7 +180,23 @@ public class AddressBookMain {
 
 		AddressBookMain final_details = new AddressBookMain();
 
-		final_details.addDetails();
+		while (true) {
+			System.out.println("Choose what you want to do: ");
+			System.out.println("1.Add details.\n2.Edit details.");
+			int choose = sc.nextInt();
+			switch (choose) {
+			case 1:
+				final_details.addDetails();
+				break;
+			case 2:
+				final_details.editDetails();
+				break;
+			default:
+				System.out.println("Choose valid option");
+				break;
+			}
+
+		}
 
 	}
 
